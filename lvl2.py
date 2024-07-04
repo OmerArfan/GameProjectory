d159f5fd59c17ec7d63092856609cd3a671dbb9b
Omer Arfan <159612176+OmerArfan@users.noreply.github.com>
2024-07-04 12:43:34 +0500

    Update v1.0.2
    CHANGES:
    ~Changed Level 2's storyline
    ~Fixed level 5's issues and actually made it possible to beat it.

--- lvl2.py
+++ lvl2.py
@@ -8,35 +8,29 @@ lo = 1
 wins = 1
 while lo < 2:
  print("your current health is" , igheal)
- print("Lawrie's current health is" , lawrihe)
+ print("S. U. 's current health is" , lawrihe)
  print("Your turn!")
  command = str(input("Type 'heal' to heal yourself, or type 'hit' to attack!"))
  if command == 'hit' and igheal > 0 and lawrihe > 0:
   lawrihe = lawrihe - yourdmg
-  print("Lawrie's turn!")
+  print("S. U. 's turn!")
   igheal = igheal - lawride
  elif command == 'heal' and igheal > 0 and lawrihe > 0:
   igheal = igheal + yourrev
-  print("Lawrie's turn!")
+  print("S. U.'s turn!")
   igheal = igheal - lawride
  elif command != 'heal' and command != 'hit' and igheal > 0 and lawrihe > 0:
   print("Type hit to attack, or type heal to heal yourself!")
- if igheal <= 0:
-  lo = lo + 1
+ if lawrihe <= 0 and igheal <= 0:
+  lo = 3
+ elif igheal <= 0:
+  lo = 3
  elif lawrihe <= 0:
-  lo = lo + 2
-
-if lo == 1:
+  lo = 2
+if lo == 3:
  wins = wins + 1
- print("LAWRIE: CURSE YOU! YOU WILL PAY FOR THIS! I AM TELLING YOU... THI- THI- TH-")
- print("OMER: FINALLY! We defeated them and got him out of here. Now we can focus more on our own skills. Come and join forces with me as we go on to explore the outside world of cruel robots abusing their power to take over the world. The struggle is endless, but is sure going to be worth it!")
+ print("S. U. : Oh wow... surely I underestimated you...")
+ print("S. U. : Well... you are free to explore around now, but be careful!")
+ print("Jerry: Wait, there are more robots out here? Well not complaining! Who doesn't love a fun challenge?")
 elif lo == 2:
- print("LAWRIE: HAHAHAHAHAHAHA! YOU'RE. NOT. MOVING. ANYWHERE!")
- print("GAME OVER")
- print("Battles won:" , wins)
- print("MADE BY OMER ARFAN")
- print("EXECUTE THE FILE AGAIN TO RETRY AND HAVE A SECOND CHANCE")
- command = str(input("Input anything to quit."))
- exit
-if wins == 2:
- print("never gonna give you up!")
+ print("Try again!")
--- lvl5.py
+++ lvl5.py
@@ -1,4 +1,5 @@
 print("Level 5.")
+wave = 1
 yourrev = 49
 yourdmg = 25
 yourheal = 197
@@ -7,7 +8,7 @@ yourrev = 25/100*yourheal
 yourrev = round(yourrev, 0)
 heale = 2
 dmgle = 2
-wins = 4
+wins = 5
 coins = 50
 rook = '1'
 rookde = 29 ## ROOKDE IS ROOKIE'S DAMAGE.
@@ -22,9 +23,7 @@ exit = False
 levelexit = False
 Upexit = False
 rookde1 = rookde
-rookde2 = rookde
 rookhe1 = rookhe
-rookhe2 = rookhe
 print("UPCOMING ROBOTS")
 print("Rookie's damage:" , rookde)
 print("Rookie's health:" , rookhe)
@@ -35,50 +34,78 @@ if command == 'p' or command == 'P':
  command = str(input("You now have to go through waves of battles!"))
  command = str(input("In the first wave, robots will attack you, but once you defeat them, another set of robots will come to attack you, which you have to defeat!"))
  print("Wave 1")
- while wins == 4:
-  if rookhe1 > 0 and rookhe2 > 0:
-   command = str(input("Type A to hit one of the mini rookies, H to heal yourself, or I to check their info!"))
-  elif rookhe1 <= 0 or rookhe2 <= 0:
-   command = str(input("Type A to hit the remaining mini rookie, H to heal yourself, or I to check his info!"))
+ while wins == 5:
+  print(igheal)
+  if rookhe1 > 0:
+   command = str(input("Type A to hit the rookie, H to heal yourself, or I to check their info!"))
   if command == 'A' or command == 'a':
-   if rookhe1 > 0 and rookhe2 > 0:
-    command = str(input("Input 1 to hit Mini Rookie 1, or input 2 to hit Mini Rookie 2."))
-    if command == '1':
-     rookhe1 = rookhe1 - 25
-    elif command == '2':
-     rookhe2 = rookhe2 - 25
-    elif rookhe1 <= 0:
-     rookhe2 = rookhe2 - 25
-    elif rookhe2 <= 0:
-     rookhe1 = rookhe1 - 25
-   if rookhe1 > 0 and rookhe2 > 0:
-    igheal = igheal - 26
-   elif rookhe2 <= 0 or rookhe1 <= 0:
-    igheal = igheal - 13
-    rookhe1 = rookhe1 - yourdmg
+   rookhe1 = rookhe1 - 25
+   igheal = igheal - rookde
+   rookhe1 = rookhe1 - yourdmg
   elif command == 'H' or command == 'h':
    if igheal < yourheal:
     igheal = igheal + yourrev
+    print(igheal)
    if igheal >= yourheal:
     igheal = yourheal
-   if rookhe1 > 0 and rookhe2 > 0:
-    igheal = igheal - 26
-   elif rookhe2 > 0 or rookhe1 > 0:
-    igheal = igheal - 13
   elif command == 'I' or command == 'i':
-   print("Mini Rookie 1's health:" , rookhe1)
-   print("Mini Rookie 2's health:" , rookhe2)
-   print("Mini Rookie's damage:" , rookde)
+   print("Rookie 1's health:" , rookhe1)
+   print("Rookie's damage:" , rookde)
   elif command != 'A' or command != 'H' or command != 'I' or command != 'a' or command != 'h' or command != 'i':
    print("Invalid function!")
   if igheal <= 0 and rookhe1 > 0 and rookhe2 > 0:
    igheal = yourheal
    rookhe1 = rookhe
    rookhe2 = rookhe
-  if rookhe1 <= 0 and rookhe2 <= 0:
+  if rookhe1 <= 0:
+   wave = wave + 1
    command = str(input("Good start!"))
    command = str(input("But remember, you haven't finished yet! We have to face the second wave of robots!"))
    print("Wave 2")
-   print("Ultra Rook: You've beat these little boys, but you saw nothing...")
-   print("You will now feel the power of the ultra!")
-   
+   command = str(input("Ultra Rook: You've beat these little boys, but you saw nothing..."))
+   command = str(input("You will now feel the power of the ultra!"))
+   command = str(input("Jerry: Well this clearly won't be easy... but worry not! I found someone who can help!"))
+   command = str(input("Ulrea Rookie: You mean this guy? *insert a man stuck in a cage*"))
+   command = str(input("Jerry: Oh no! Well we have to destroy the cage first!"))
+   cagehealth = 110
+   urookhe1 = urookhe
+   while wave == 2:
+    if cagehealth > 0:
+     command = str(input("Type A to hit either the ultra rookie or the cage, H to heal yourself, or I to check their info!"))
+    if command == 'A' or command == 'a':
+      command = str(input("Input U to hit the ultra rookie, or input C to hit the cage."))
+      if command == 'C' or command == 'c':
+       cagehealth = cagehealth - 25
+      elif command == 'U' or command == 'u':
+       urookhe1 = urookhe1- 25
+      igheal = igheal - urookde
+    elif command == 'H' or command == 'h':
+     if igheal < yourheal:
+      igheal = igheal + yourrev
+      print("Health currently:" , igheal)
+     if igheal >= yourheal:
+      igheal = yourheal
+     igheal = igheal - urookde
+    elif command == 'I' or command == 'i':
+     print("Ultra Rookie's health:" , urookhe1)
+     print("Cage's health:" , cagehealth)
+     print("Ultra Rookie's damage:" , urookde)
+    elif command != 'A' or command != 'H' or command != 'I' or command != 'a' or command != 'h' or command != 'i':
+     print("Invalid function!")
+    if cagehealth <= 0:
+     command = str(input("WIzard: Unleahing the power of MAGIC!"))
+     command = str(input("Ultra Rookie: Wait wait wait wait- NOOOOOOOO-"))
+     urookhe1 = 0
+     wave = 3
+     wins = 6
+elif command != 'p' or command != 'P':
+ print("invalid option!")
+if wins == 6:
+ wizhe = 110
+ wizde = 21
+ print("NEW CHARACTER UNLOCKED!")
+ print("Wizard")
+ print("Common")
+ print("STATS")
+ print("Health:" , wizhe)
+ print("Damage:" , wizde)
